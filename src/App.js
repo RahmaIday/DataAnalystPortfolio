d780c4fc0ccc7a892164112984ae02de4fc0d8e8
RahmaIday <rahma.idayath@outlook.com>
2024-02-20 17:20:33 +1100

    greeting section base completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import landingImage from './assets/images/landingImage.jpg'
+import wave from './assets/images/wave.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='greeting-main'>
+        <div className='greeting-text-div'>
+          <h1 className='greeting-text'>Hello, I'm Rahma 
+            <span class="wave-emoji">
+              <img
+                alt="👋"
+                draggable="false"
+                src={wave}
+                class="wave-img"
+              />
+            </span>
+          </h1>
+          <div className='greeting-text-p'>A ..... data analyst with experience in ....... </div>
+        </div>
+        <div className='greeting-image-div'>
+          <img className='data-image' src={landingImage}/>
+        </div>
+
+      </div>
     </div>
   );
 }
