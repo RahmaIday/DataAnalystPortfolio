8bedfcba2bd91d59d05489ec9501f8e6fd186d9e
RahmaIday <rahma.idayath@outlook.com>
2024-02-21 11:50:16 +1100

    extracting into sections

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import './App.css';
-import landingImage from './assets/images/landingImage.jpg'
-import wave from './assets/images/wave.png'
 import NavBar from './components/navbar';
+import Greeting from './sections/greeting';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
-      <div className='greeting-main'>
-        <div className='greeting-text-div'>
-          <h1 className='greeting-text'>Hello, I'm Rahma 
-            <span class="wave-emoji">
-              <img
-                alt="👋"
-                draggable="false"
-                src={wave}
-                class="wave-img"
-              />
-            </span>
-          </h1>
-          <div className='greeting-text-p'>A ..... data analyst with experience in ....... </div>
-        </div>
-        <div className='greeting-image-div'>
-          <img alt='landingImage' className='data-image' src={landingImage}/>
-        </div>
-
-      </div>
+      <Greeting></Greeting>
     </div>
   );
 }
---
+++ src/sections/greeting.js
@@ -0,0 +1,28 @@
+import landingImage from '../assets/images/landingImage.jpg'
+import wave from '../assets/images/wave.png'
+import './greeting.css';
+
+function Greeting(){
+    return(
+        <div className='greeting-main'>
+            <div className='greeting-text-div'>
+            <h1 className='greeting-text'>Hello, I'm Rahma 
+                <span class="wave-emoji">
+                <img
+                    alt="👋"
+                    draggable="false"
+                    src={wave}
+                    class="wave-img"
+                />
+                </span>
+            </h1>
+            <div className='greeting-text-p'>A ..... data analyst with experience in ....... </div>
+            </div>
+            <div className='greeting-image-div'>
+            <img alt='landingImage' className='data-image' src={landingImage}/>
+            </div>
+        </div>
+    )
+}
+
+export default Greeting;
