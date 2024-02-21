f3e2fb58aa8a212a39843826f3003a82e46d5b5c
RahmaIday <rahma.idayath@outlook.com>
2024-02-21 11:33:48 +1100

    adding icons to navbar options

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import './navbar.css';
+import logo from '../assets/images/logo.png';
+import resumeIcon from '../assets/images/resume-icon.svg'
+import skillsIcon from '../assets/images/skills-icon.svg'
+import projectsIcon from '../assets/images/projects-icon.svg'
 
 function NavBar() {
     const [isNavbarExpanded, setIsNavbarExpanded] = useState(false);
@@ -21,8 +25,8 @@ function NavBar() {
     return (
         <nav className="navbar-container container">
             <a href="/" className="home-link">
-                <div className="navbar-logo"></div>
-                Rahma Iday
+                <div className="navbar-logo"><img className='navbar-logo-img' src={logo}/></div>
+                RAHMA.IDAY
             </a>
 
             <button
@@ -39,9 +43,15 @@ function NavBar() {
 
             <div id="navbar-menu" aria-labelledby="navbar-toggle">
                 <ul className={`navbar-links ${isNavbarExpanded ? 'expanded' : ''}`}>
-                    <li className="navbar-item"><a className="navbar-link" href="/about">Projects</a></li>
-                    <li className="navbar-item"><a className="navbar-link" href="/blog">Skills</a></li>
-                    <li className="navbar-item"><a className="navbar-link" href="/careers">Resume</a></li>
+                    <li className="navbar-item"><a className="navbar-link" href="/about">
+                        <div className="item-logo-div"><img className='item-img' src={projectsIcon}/></div>
+                        Projects</a></li>
+                    <li className="navbar-item"><a className="navbar-link" href="/blog">
+                        <div className="item-logo-div"><img className='item-img' src={skillsIcon}/></div>
+                        Skills</a></li>
+                    <li className="navbar-item"><a className="navbar-link" href="/careers">
+                        <div className="item-logo-div"><img className='item-img' src={resumeIcon}/></div>
+                        Resume</a></li>
                 </ul>
             </div>
         </nav>
