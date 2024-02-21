4e9d0584a9616555fed7830ac3d80b7bf2f1dab3
RahmaIday <rahma.idayath@outlook.com>
2024-02-21 12:07:55 +1100

    Projects section created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './components/navbar';
 import Greeting from './sections/greeting';
+import Projects from './sections/projects';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
       <Greeting></Greeting>
+      <Projects></Projects>
     </div>
   );
 }
---
+++ src/sections/projects.js
@@ -0,0 +1,14 @@
+import './projects.css'
+
+function Projects(){
+    return(
+        <div className='project-main-div'>
+            <h1 className='project-title'>Projects</h1>
+            <div className='repo-cards-div-main'>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Projects;
