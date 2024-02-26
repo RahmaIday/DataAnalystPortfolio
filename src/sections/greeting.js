3f69a6e0edb665f20e31dc0b1e4ae1f788f62ff4
RahmaIday <rahma.idayath@outlook.com>
2024-02-26 18:38:03 +1100

    spacing fix

--- src/sections/greeting.js
+++ src/sections/greeting.js
@@ -6,20 +6,20 @@ function Greeting(){
     return(
         <div className='greeting-main'>
             <div className='greeting-text-div'>
-            <h1 className='greeting-text'>Hello, I'm Rahma 
-                <span class="wave-emoji">
-                <img
-                    alt="👋"
-                    draggable="false"
-                    src={wave}
-                    class="wave-img"
-                />
-                </span>
-            </h1>
-            <div className='greeting-text-p'>A ..... data analyst with experience in ....... </div>
+                <h1 className='greeting-text'>Hello, I'm Rahma 
+                    <span class="wave-emoji">
+                    <img
+                        alt="👋"
+                        draggable="false"
+                        src={wave}
+                        class="wave-img"
+                    />
+                    </span>
+                </h1>
+                <div className='greeting-text-p'>A ..... data analyst with experience in ....... </div>
             </div>
             <div className='greeting-image-div'>
-            <img alt='landingImage' className='data-image' src={landingImage}/>
+            <img alt='landingImage' className='data-image' src={landingImage}/>            
             </div>
         </div>
     )
