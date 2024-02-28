aeeaab2226f981018c3535eb6996dfb19e6f7e56
RahmaIday <rahma.idayath@outlook.com>
2024-02-28 23:39:30 +1100

    project cards revamp

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -5,26 +5,22 @@ function ProjectCard(props) {
     const { project } = props;
 
     return (
-        <div className='project-card-div'>
-            <div>
-                <a target="_blank" href={project.projectLink} className='project-card-title'>
-                    {project.title}
-                </a>
-                <br></br>
-                <br></br>
-                <div className='project-card-desc'>{project.description}</div>
-                <br></br>
-                {/* <div className='skills-title'>Skills:</div> */}
-                <div className='skills-div-main'>
-                    {project.skills.map((skill, index) => (
-                        <div key={index} className='skill'>{skill}</div>
-                    ))}
-                </div>
-            </div>
+        <a target="_blank" href={project.projectLink} className='project-card-div'>
             <div className='project-card-img-div'>
                 <img className='project-card-img' alt='projectHero' src={project.imageUrl}></img>
             </div>
-        </div>
+            <div className='project-text-container'>
+                <div className='skills-div-main'>
+                        {project.skills.map((skill, index) => (
+                            <div key={index} className='skill'>{skill}</div>
+                        ))}
+                </div>
+                <div className='project-card-title'>
+                        {project.title}
+                </div>
+                <div className='project-card-desc'>{project.description}</div>
+            </div>
+        </a>
     );
 }
 
--- src/data/projectDetails.js
+++ src/data/projectDetails.js
@@ -1,15 +1,22 @@
-import movieImage from './moviess4.png'
+import movieImage from './MovieProjectImage.png'
 import fakeImage from './moviess5.png'
 
 const projectDetails = {
     project1: {
         title: "Movie Correlation",
-        description: "Exploring Correlation in a Movie Dataset with Python in Jupyter Notebooks",
+        description: "Data Cleaning + Exploring Correlation in a Movie Dataset with Python in Jupyter Notebooks",
         imageUrl: movieImage,
         projectLink: "https://github.com/RahmaIday/Data-Analysis-Projects/blob/main/Movie%20Correlation%20Project.ipynb",
-        skills: ["Python","Data Cleaning","Pandas","Numpy","Seaborn","Jupyter Notebook"]
+        skills: ["Python","Pandas","Numpy","Seaborn","Jupyter Notebook"]
     },
     project2: {
+        title: "Machine Learning",
+        description: "This is the description of Project 2.",
+        imageUrl: fakeImage,
+        projectLink: "https://github.com/RahmaIday/Data-Analysis-Projects/blob/main/Movie%20Correlation%20Project.ipynb",
+        skills: ["Deceit", "Fraud"]
+    },
+    project3: {
         title: "Machine Learning",
         description: "This is the description of Project 2.",
         imageUrl: fakeImage,
