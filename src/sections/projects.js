1cce3602202455ad6bc5fd2673a81f50a95fee68
RahmaIday <rahma.idayath@outlook.com>
2024-02-29 14:15:26 +1100

    extracting common css styling to sections.css

--- src/sections/projects.js
+++ src/sections/projects.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './projects.css';
+import './section.css'
+
 import projectDetails from '../data/projectDetails';
 import ProjectCard from '../components/projectCard';
 
 function Projects() {
     
     return (
-        <div className='project-main-div'>
-            <h1 id="projects" className='project-title'>Projects</h1>
+        <div className='section-main-div'>
+            <h1 id="projects" className='section-title'>Projects</h1>
             <div className='repo-cards-div-main'>
                 {Object.keys(projectDetails).map((projectKey) => {
                     const project = projectDetails[projectKey];
