a0e3681ab4386a0841a04bbeeabe4216d1d1bca4
RahmaIday <rahma.idayath@outlook.com>
2024-02-29 14:15:00 +1100

    creating education section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import NavBar from './components/navbar';
+import Education from './sections/education';
 import Greeting from './sections/greeting';
 import Projects from './sections/projects';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <NavBar></NavBar>
       <Greeting></Greeting>
+      <Education></Education>
       <Projects></Projects>
     </div>
   );
---
+++ src/sections/education.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './education.css'
+import './section.css'
+
+function Education(){
+
+    return(
+        <div className='section-main-div'>
+            <h1 id="education" className='section-title'>Education</h1>
+
+        </div>
+    );
+}
+
+export default Education
