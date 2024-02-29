b94b14d0e6a1292bab754f2dd56a703e19aa6736
RahmaIday <rahma.idayath@outlook.com>
2024-02-29 13:54:16 +1100

    Click to view project overlay

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -8,20 +8,24 @@ function ProjectCard(props) {
         <a target="_blank" href={project.projectLink} className='project-card-div'>
             <div className='project-card-img-div'>
                 <img className='project-card-img' alt='projectHero' src={project.imageUrl}></img>
+                <div className='project-card-img-overlay'>
+                    Click to View Project
+                </div>
             </div>
             <div className='project-text-container'>
                 <div className='skills-div-main'>
-                        {project.skills.map((skill, index) => (
-                            <div key={index} className='skill'>{skill}</div>
-                        ))}
+                    {project.skills.map((skill, index) => (
+                        <div key={index} className='skill'>{skill}</div>
+                    ))}
                 </div>
                 <div className='project-card-title'>
-                        {project.title}
+                    {project.title}
                 </div>
                 <div className='project-card-desc'>{project.description}</div>
             </div>
         </a>
     );
 }
+
 
 export default ProjectCard;
