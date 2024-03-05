9f31f1dc1b20d3449700cd9f8b138d8768167507
RahmaIday <rahma.idayath@outlook.com>
2024-03-05 13:57:26 +1100

    skills details initial commit

---
+++ src/details/skillDetails.js
@@ -0,0 +1,20 @@
+import NumPy from '../assets/images/skills/NumPy.png';
+import pandas from '../assets/images/skills/pandas.png';
+import seaborn from '../assets/images/skills/seaborn.png';
+
+const skillDetails= {
+    skill1: {
+        title: "NumPy",
+        imageUrl: {NumPy}
+    },
+    skill2: {
+        title: "pandas",
+        imageUrl: {pandas}
+    },
+    skill3: {
+        title: "seaborn",
+        imageUrl: {seaborn}
+    }
+};
+
+export default skillDetails;
