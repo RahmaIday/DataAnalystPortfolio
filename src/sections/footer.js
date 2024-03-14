cdae4a1ed558e913bf06ae24657d390a5c11ffa4
RahmaIday <rahma.idayath@outlook.com>
2024-03-14 15:20:53 +1100

    adding socials to footer

--- src/sections/footer.js
+++ src/sections/footer.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './footer.css';
+import Socials from "../components/socials";
 
 function Footer(){
     return(
         <div className="footer-main-div">
             <div className="footer-credits">Developed by Rahma Idayathulla</div>
+            <Socials></Socials>
         </div>
     );
 
