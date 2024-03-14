0a0e7890f4dd583c51a714f2920357ca33357b68
RahmaIday <rahma.idayath@outlook.com>
2024-03-14 15:01:38 +1100

    commenting out

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './navbar.css';
 import logo from '../assets/images/logo.png';
-import resumeIcon from '../assets/images/resume-icon.svg'
-import skillsIcon from '../assets/images/skills-icon.svg'
+// import resumeIcon from '../assets/images/resume-icon.svg'
+// import skillsIcon from '../assets/images/skills-icon.svg'
 import projectsIcon from '../assets/images/projects-icon.svg'
 
 function NavBar() {
@@ -25,7 +25,7 @@ function NavBar() {
     return (
         <nav className="navbar-container container">
             <a href="/DataAnalystPortfolio" className="home-link">
-                <div className="navbar-logo"><img className='navbar-logo-img' src={logo}/></div>
+                <div className="navbar-logo"><img alt='home-link-logo' className='navbar-logo-img' src={logo}/></div>
                 RAHMA.IDAY
             </a>
 
@@ -44,7 +44,7 @@ function NavBar() {
             <div id="navbar-menu" aria-labelledby="navbar-toggle">
                 <ul className={`navbar-links ${isNavbarExpanded ? 'expanded' : ''}`}>
                     <li className="navbar-item"><a className="navbar-link" href="#projects">
-                        <div className="item-logo-div"><img className='item-img' src={projectsIcon}/></div>
+                        <div className="item-logo-div"><img alt='project-link-logo' className='item-img' src={projectsIcon}/></div>
                         Projects</a></li>
                     {/* <li className="navbar-item"><a className="navbar-link" href="/blog">
                         <div className="item-logo-div"><img className='item-img' src={skillsIcon}/></div>
