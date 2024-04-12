2bf820661da046d26d53b6f6b69b2349da330597
RahmaIday <rahma.idayath@outlook.com>
2024-04-13 01:28:46 +1000

    updated new project to project section

--- src/details/projectDetails.js
+++ src/details/projectDetails.js
@@ -1,21 +1,22 @@
 import movieImage from './MovieProjectImage.png'
-// import fakeImage from './tbd.jpg'
+import life_expectancy from './life_expectancy.png'
 
 const projectDetails = {
     project1: {
+        title: "Building Linear Regression Models",
+        description: "This project involved completing EDA(Exploratory Data Analysis) and then building models that would accurately predict Life Expectancy (in years) based on input features.",
+        imageUrl: life_expectancy,
+        projectLink: "https://github.com/RahmaIday/Data-Analysis-Projects/blob/main/ML_Linear_Regression.ipynb",
+        skills: ["Scikit-Learn", "Data Pre-processing","EDA","Feature Engineering", "Cross-Validation", "Regularization","Hyperparameter Tuning", "Model Evaluation" ]
+    },
+    project2: {
         title: "Movie Correlation",
         description: "Data Cleaning + Exploring Correlation in a Movie Dataset with Python in Jupyter Notebooks",
         imageUrl: movieImage,
         projectLink: "https://github.com/RahmaIday/Data-Analysis-Projects/blob/main/Movie%20Correlation%20Project.ipynb",
         skills: ["Python","Pandas","NumPy","Seaborn","Jupyter Notebook"]
     }
-    // project2: {
-    //     title: "Machine Learning",
-    //     description: "This is the description of Project 2.",
-    //     imageUrl: fakeImage,
-    //     projectLink: "https://github.com/RahmaIday/Data-Analysis-Projects/blob/main/Movie%20Correlation%20Project.ipynb",
-    //     skills: ["Deceit", "Fraud"]
-    // },
+    
     // project3: {
     //     title: "Machine Learning",
     //     description: "This is the description of Project 2.",
